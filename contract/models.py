f909e21131c70240b2e3293f86659e17a30ef2b8
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-10-26 16:36:02 +0700

    Add BaseResponse

--- contract/models.py
+++ contract/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+class BaseResponse():
+    success = False
+    data = {}
+    def __str__(self):
+        return self.data
+    def serialize(self):
+        return {
+            "success": self.success,
+            "data": self.data
+        }
 
-# Create your models here.
+def build_success_response(data):
+    response = BaseResponse()
+    response.success = True
+    response.data = data
+    return response
+
+def build_fail_response(data):
+    response = BaseResponse()
+    response.success = False
+    response.data = data
+    return response
